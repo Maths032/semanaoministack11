8f874e49eb3538e9c28e2b8d9a7743819a12953c
Maths032 <matheus.ramos.silva.santos@gmail.com>
2020-04-05 15:39:49 -0300

    correçẽes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,12 @@ const express = require('express');
 const routes = require('./routes.js')
 const cors = require('cors')
 const app = express();
+const { errors } = require('celebrate')
 
 app.use(cors({
   // origin: 'sitepermitido para acesso'
 }))
+app.use(routes);
+app.use(errors());
 app.listen(3333);
-app.use(routes)
+
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,18 +3,27 @@ const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
 const SessionController = require('./controllers/SessionController')
+const {celebrate, Segments, Joi} = require('celebrate')
 
 const routes = express.Router();
 routes.use(express.json())
 
 routes.post('/sessions', SessionController.create)
+routes.get('/profile',CELEBRATE , ProfileController.index)
 
-routes.get('/profile', ProfileController.index)
 routes.post('/incidents', IncidentController.create)
 routes.get('/incidents', IncidentController.index)
 routes.delete('/incidents/:id', IncidentController.delete)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
+routes.post('/ongs',celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngController.create)
 
 module.exports = routes;
